a0644cceb3ffa0a45053299b7b46c196c17a6e19
guanguancode <1271835875@qq.com>
2023-04-26 12:47:17 +0800

    触发长按效果

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,11 +1,11 @@
-import { defineComponent, PropType, onUpdated } from 'vue';
-import s from './Tags.module.scss';
-import { Resources, Tag } from '../../env';
-import { http } from '../../shared/Http';
-import { useTags } from '../../shared/useTags';
-import { Icon } from '../../shared/Icon';
-import { Button } from '../../shared/Button';
+import { defineComponent, onUpdated, PropType, ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import { Button } from '../../shared/Button';
+import { http } from '../../shared/Http';
+import { Icon } from '../../shared/Icon';
+import { useTags } from '../../shared/useTags';
+import { Resources, Tag } from '../../env';
+import s from './Tags.module.scss';
 export const Tags = defineComponent({
   props: {
     kind: {
@@ -26,9 +26,31 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => {
       context.emit('update:selected', tag.id);
     };
+    const timer = ref<number>()
+    const currentTag = ref<HTMLDivElement>()
+
+    const onLongPress = ()=>{
+      console.log('长按')
+    }
+    const onTouchStart = (e: TouchEvent) => {
+      currentTag.value = e.currentTarget as HTMLDivElement
+      timer.value = window.setTimeout(()=>{
+        onLongPress()
+      }, 500)
+    }
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value)
+    }
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
+      if(currentTag.value !== pointedElement &&
+        currentTag.value?.contains(pointedElement) === false){
+        clearTimeout(timer.value)
+      }
+    }
     return () => (
       <>
-        <div class={s.tags_wrapper}>
+        <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
           <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
             <div class={s.sign}>
               <Icon name="add" class={s.createTag} />
@@ -39,6 +61,8 @@ export const Tags = defineComponent({
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : '']}
               onClick={() => onSelect(tag)}
+              onTouchstart={onTouchStart}
+              onTouchend={onTouchEnd}
             >
               <div class={s.sign}>{tag.sign}</div>
               <div class={s.name}>{tag.name}</div>
