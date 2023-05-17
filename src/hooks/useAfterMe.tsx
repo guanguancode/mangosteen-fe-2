9c70011731f53cf1c7387a4183282b795f049020
guanguancode <1271835875@qq.com>
2023-05-17 11:23:11 +0800

    解决 Vue 关于 onMounted 中有未处理的 error 的警告

--- src/hooks/useAfterMe.tsx
+++ src/hooks/useAfterMe.tsx
@@ -3,8 +3,8 @@ import { useMeStore } from "../stores/useMeStore";
 
 export const useAfterMe = (fn: () => void) => {
     const meStore = useMeStore()
-    onMounted(async () => {
-        await meStore.mePromise
+    onMounted( () => {
+      meStore.mePromise!.then(fn, ()=> undefined)
         fn()
     })
 }
